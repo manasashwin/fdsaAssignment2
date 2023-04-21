28f8f10042dfe0be91d3d8a5097a75b3d800f66a
Manas Ashwin <f20211908@hyderabad.bits-pilani.ac.in>
2023-04-21 17:00:59 +0530

    Question 1 and Question2 also done

--- Question1.cpp
+++ Question1.cpp
@@ -1,46 +1,69 @@
 #include <iostream>
 #include <vector>
 
+// using the standard namespace
 using namespace std;
+
+// define a struct Node to hold the binary tree nodes
 struct Node
 {
     int data;
     struct Node *left, *right;
     Node(int d){
-        data = d;
-        left = right = NULL;
+    data = d;
+    left = right = NULL;
     }
 };
 
+// function to print the preorder traversal of a binary tree
 void printPreorder(Node *root)
 {
+    // if the root is not null
     if (root != NULL)
     {
-        cout << root->data << " ";
+    // print the current node
+    cout << root->data << " ";
+
+        // recursively print the left subtree
         printPreorder(root->left);
+
+        // recursively print the right subtree
         printPreorder(root->right);
     }
 }
 
+// function to generate all binary trees with the same inorder traversal
 vector<Node *> treesWithSameInorder( int arr[], int start, int end){
+    // create a vector to hold all the generated trees
     vector<Node *> allTrees;
 
+    // if start is greater than end, add null to the vector and return it
     if (start > end)
     {
         allTrees.push_back(NULL);
         return allTrees;
     }
 
+    // loop through each element of the array
     for(int i = start; i <= end; i++){
+        // recursively generate all possible left subtrees
         vector<Node *> lTrees = treesWithSameInorder(arr, start, i-1);
+
+        // recursively generate all possible right subtrees
         vector<Node *> rTrees = treesWithSameInorder(arr, i+1, end);
 
+        // loop through all possible combinations of left and right subtrees
         for (int j = 0; j<lTrees.size(); j++){
             for (int k = 0; k < rTrees.size(); k++)
             {
+                // create a new node with the current element as data
                 Node* newNode = new Node(arr[i]);
+
+                // set the left and right subtrees to the ones generated recursively
                 newNode->left = lTrees[j];
                 newNode->right=rTrees[k];
+
+                // add the new tree to the vector of all trees
                 allTrees.push_back(newNode);
                 
             }
@@ -48,16 +71,20 @@ vector<Node *> treesWithSameInorder( int arr[], int start, int end){
         }
         
     }
+    // return the vector of all generated trees
     return allTrees;
-} 
-
+}
 
 int main()
 {
+    // read the number of test cases
     int n;
-    cin >> n; 
+    cin >> n;
+
+    // loop through each test case
     for (int j = 0; j < n; j++)
     {
+        // read the input array
         int in[INT16_MAX];
         int n;
         cin >> n;
@@ -65,8 +92,14 @@ int main()
         {
             cin >> in[i];
         }
+
+        // generate all possible binary trees with the given inorder traversal
         vector<Node *> trees = treesWithSameInorder(in, 0, n-1);
+
+        // print the total number of generated trees
         cout << "Total Binary trees are: " << trees.size() << endl;
+
+        // print the preorder traversal of each generated tree
         cout << "Preorder traversals of different "
             << "possible Binary Trees are \n";
         for (int i = 0; i < trees.size(); i++)
@@ -75,6 +108,4 @@ int main()
             cout << endl;
         }
     }
-    
-    
 }
--- Question2.cpp
+++ Question2.cpp
@@ -3,35 +3,37 @@
 #include <queue>
 using namespace std;
 
-bool greaterh(int i1, int i2)
-{
-    return (i1 > i2);
-}
 int main()
 {
     int t;
     cin >> t;
     while (t--){
         int n, k;
+        cout<<"enter number of bags :";
         cin >> n;
+        cout<<"enter number of minutes allowed :";
         cin >> k;
         vector<int> arr;
         priority_queue<int> pq;
+        cout<<"enter number of balls :";
+
+        // enter number of balls in each bag and push into priority queue
         for (int i = 0; i < n; i++)
         {
             int l;
             cin >> l;
             pq.push(l);
         }
-        int i = 0;
+        
         int balls = 0;
         while(k--){
             int newBalls = pq.top();
-            balls += newBalls;
+            balls += newBalls; 
+            // adds the top of the priority queue i.e the largest ball containing bag into the balls var
             pq.pop();
-            pq.push(floor(newBalls/2));
-            i++;
+            pq.push(floor(newBalls/2)); // pushes half of the previous value to priority queue
+        
         }
-        cout << balls << endl;
+        cout << "Maximum balls that you can collect " << balls << endl;
     }
 }
